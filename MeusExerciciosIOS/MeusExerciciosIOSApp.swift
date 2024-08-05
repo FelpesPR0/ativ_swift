013b9eb5534eb9f3e188b7f4c5d23dfb2215dea7
FelpesPR0 <f.olimpio.olimpio@gmail.com>
2024-08-06 00:30:51 +0100

    Initial Commit

---
+++ MeusExerciciosIOS/MeusExerciciosIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeusExerciciosIOSApp.swift
+//  MeusExerciciosIOS
+//
+//  Created by Felipe Silva on 06/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct MeusExerciciosIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
